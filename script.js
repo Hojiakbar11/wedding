4a76c656e612eb486cbaa2f9fa43417945c12d9a
Azizovh <hojiakbarazizov1603@gmail.com>
2024-10-12 08:23:04 +0300

    Added new content

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-const weddingDate = new Date('August 15, 2024 06:43:00').getTime();
+function countdown() {
+    const targetDate = new Date('2024-10-22T00:00:00').getTime();
+    const x = setInterval(function () {
+        const now = new Date().getTime();
+        const distance = targetDate - now;
 
-function updateCountdown() {
-    const now = new Date().getTime();
-    const distance = weddingDate - now;
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById('days').innerHTML = days + " <span>kun</span>";
-    document.getElementById('hours').innerHTML = hours + " <span>soat</span>";
-    document.getElementById('minutes').innerHTML = minutes + " <span>daqiqalar</span>";
-    document.getElementById('seconds').innerHTML = seconds + " <span>soniyalar</span>";
+        document.getElementById('days').innerHTML = days + ' <span>kun</span>';
+        document.getElementById('hours').innerHTML = hours + ' <span>soat</span>';
+        document.getElementById('minutes').innerHTML = minutes + ' <span>daqiqalar</span>';
+        document.getElementById('seconds').innerHTML = seconds + ' <span>soniyalar</span>';
 
-    if (distance < 0) {
-        clearInterval(interval);
-        document.querySelector('.countdown').innerHTML = "To'y boshlandi!";
-    }
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById('days').innerHTML = '00 <span>kun</span>';
+            document.getElementById('hours').innerHTML = '00 <span>soat</span>';
+            document.getElementById('minutes').innerHTML = '00 <span>daqiqalar</span>';
+            document.getElementById('seconds').innerHTML = '00 <span>soniyalar</span>';
+        }
+    }, 1000);
 }
 
-const interval = setInterval(updateCountdown, 1000);
+countdown();
